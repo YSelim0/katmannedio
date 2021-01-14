c9bf2badc8ab11ef235cc1507f8317139e6fa008
Yavuz Selim Şen <yavuzselimice@gmail.com>
2021-01-15 01:24:44 +0300

    Vue  created.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="App">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'App',
+    data(){
+        return {
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+@import url('./css/main.css');
+
+</style>
